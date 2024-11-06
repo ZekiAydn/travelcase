12cb441255b9711e5286de9099fcdb2dfa330ebb
Zeki Aydın <105093184+ZekiAydn@users.noreply.github.com>
2024-11-06 13:36:56 +0300

    fix data bugs

--- src/components/FilterSidebar.tsx
+++ src/components/FilterSidebar.tsx
@@ -1,147 +1,151 @@
-import { Input, Select, Button, Radio, DatePicker } from "antd";
+import { Input, Select, DatePicker, Radio, Button } from "antd";
 import { CalendarOutlined, EnvironmentOutlined, GlobalOutlined, MoonOutlined, TeamOutlined, FilterOutlined, StarFilled } from "@ant-design/icons";
 import { useTranslation } from 'next-i18next';
-import HotelConceptSelector from "@/components/ConceptSelector";
-import React, {useEffect, useState} from "react";
-import dayjs from "dayjs";
-
+import dayjs, {Dayjs} from "dayjs";
+import React, {useState} from "react";
 
 const { Option } = Select;
 
+interface FilterParams {
+    from: string;
+    destination: string;
+    date: string;
+    nights: string;
+    people: string;
+    rating: string;
+}
+
 interface FilterSidebarProps {
-    filters: {
-        from: string;
-        destination: string;
-        date: string;
-        nights: string;
-        people: string;
-    };
-    setFilters: (key: string, value: string) => void;
+    filters: FilterParams;
+    setFilters: (key: keyof FilterParams, value: string) => void;
     nightOptions: string[];
     peopleOptions: string[];
     filtersOpen: boolean;
     setFiltersOpen: (open: boolean) => void;
 }
 
+
 const FilterSidebar: React.FC<FilterSidebarProps> = ({ filters, setFilters, nightOptions, peopleOptions, filtersOpen, setFiltersOpen }) => {
     const { t } = useTranslation('common');
-    const initialDate = dayjs(filters.date);
-    const [selectedDate, setSelectedDate] = useState(initialDate);
+    const [selectedDate, setSelectedDate] = useState<Dayjs | null>(dayjs(filters.date));
 
-    useEffect(() => {
-        console.log('DATE =>',filters.date)
-    }, []);
+
+    const handleDateChange = (date: Dayjs | null) => {
+        setSelectedDate(date);
+        if (date) {
+            setFilters("date", date.format("YYYY-MM-DD"));
+        } else {
+            setFilters("date", "");
+        }
+    };
+
     return (
         <>
-        <div className="lg:hidden flex items-center self-end justify-between">
-            <Button
-                type="text"
-                icon={<FilterOutlined/>}
-                onClick={() => setFiltersOpen(!filtersOpen)}
-            >
-                {filtersOpen ? t('hideFilters') : t('showFilters')}
-            </Button>
-        </div>
-    <div className={`${filtersOpen ? "block" : "hidden"} lg:block lg:w-1/4 bg-white border p-6 rounded-lg`}>
-
-        <h2 className="text-xs">{t('filter')}</h2>
-
-        <div className="mb-4">
-            <Input
-                size="large"
-                placeholder={t('from')}
-                prefix={<EnvironmentOutlined style={{color: "#4A5568"}}/>}
-                className="rounded-md"
-                    value={filters.from}
-                    onChange={(e) => setFilters("from", e.target.value)}
-                />
+            <div className="lg:hidden flex items-center justify-end p-4">
+                <Button
+                    type="text"
+                    icon={<FilterOutlined />}
+                    onClick={() => setFiltersOpen(!filtersOpen)}
+                >
+                    {filtersOpen ? t('hideFilters') : t('showFilters')}
+                </Button>
             </div>
+            <div className={`filter-sidebar ${filtersOpen ? "block" : "hidden"} lg:block`}>
+                <h2 className="filter-title">{t('filter')}</h2>
 
-            <div className="mb-4">
-                <Input
-                    size="large"
-                    placeholder={t('destination')}
-                    prefix={<GlobalOutlined style={{ color: "#4A5568" }} />}
-                    className="rounded-md"
-                    value={filters.destination}
-                    onChange={(e) => setFilters("destination", e.target.value)}
-                />
-            </div>
-
-            <div className="mb-4">
-                <label className="text-xs mb-4">{t('participants')}</label>
-                <div className="flex items-center space-x-2 w-full sm:w-auto border rounded-lg">
-                    <TeamOutlined style={{ color: "#4A5568", marginLeft: 14 }} />
-                    <Select
+                {/* From Input */}
+                <div className="filter-item">
+                    <Input
                         size="large"
-                        className="w-full"
-                        variant="borderless"
-                        value={filters.people}
-                        onChange={(val) => setFilters("people", val)}
-                    >
-                        {peopleOptions.map((peopleOption) => (
-                            <Option key={peopleOption} value={peopleOption}>
-                                {peopleOption}
-                            </Option>
-                        ))}
-                    </Select>
-                </div>
-            </div>
-
-            <div className="mb-4">
-                <label className="text-xs mb-4">{t('date')}</label>
-                <div className="flex items-center space-x-2 w-full sm:w-auto border rounded-lg">
-                    <CalendarOutlined className="text-gray-700 text-xl ml-3" />
-                    <DatePicker
-                        suffixIcon={null}
-                        variant="borderless"
-                        size="large"
-                        placeholder={t('date')}
-                        className="text-gray-700 text-xs w-full"
-                        format="DD MMM"
-                        value={selectedDate}
-                        onChange={(v) => setSelectedDate(v)}
+                        placeholder={t('from')}
+                        prefix={<EnvironmentOutlined className="filter-icon" />}
+                        value={filters.from}
+                        onChange={(e) => setFilters("from", e.target.value)}
                     />
                 </div>
-            </div>
 
-            <div className="mb-4">
-                <label className="text-xs mb-4">{t('nights')}</label>
-                <div className="flex items-center space-x-2 w-full sm:w-auto border rounded-lg">
-                    <MoonOutlined style={{ color: "#4A5568", marginLeft: 14 }} />
-                    <Select
+                {/* Destination Input */}
+                <div className="filter-item">
+                    <Input
                         size="large"
-                        className="w-full"
-                        value={filters.nights}
-                        onChange={(val) => setFilters("nights", val)}
-                        variant="borderless"
-                    >
-                        {nightOptions.map((nightOption) => (
-                            <Option key={nightOption} value={nightOption}>
-                                {nightOption}
-                            </Option>
+                        placeholder={t('destination')}
+                        prefix={<GlobalOutlined className="filter-icon" />}
+                        value={filters.destination}
+                        onChange={(e) => setFilters("destination", e.target.value)}
+                    />
+                </div>
+
+                {/* Participants Select */}
+                <div className="filter-item">
+                    <label className="filter-label">{t('participants')}</label>
+                    <div className="filter-input">
+                        <TeamOutlined className="filter-icon" />
+                        <Select
+                            size="large"
+                            className="w-full"
+                            value={filters.people}
+                            onChange={(val) => setFilters("people", val)}
+                        >
+                            {peopleOptions.map((option) => (
+                                <Option key={option} value={option}>
+                                    {option}
+                                </Option>
+                            ))}
+                        </Select>
+                    </div>
+                </div>
+
+                {/* Date Picker */}
+                <div className="filter-item">
+                    <label className="filter-label">{t('date')}</label>
+                    <div className="filter-input">
+                        <CalendarOutlined className="filter-icon" />
+                        <DatePicker
+                            size="large"
+                            placeholder={t('date')}
+                            format="DD MMM"
+                            value={selectedDate}
+                            onChange={(date) =>handleDateChange(date)}
+                            className="w-full"
+                        />
+                    </div>
+                </div>
+
+                {/* Nights Select */}
+                <div className="filter-item">
+                    <label className="filter-label">{t('nights')}</label>
+                    <div className="filter-input">
+                        <MoonOutlined className="filter-icon" />
+                        <Select
+                            size="large"
+                            value={filters.nights}
+                            onChange={(val) => setFilters("nights", val)}
+                            className="w-full"
+                        >
+                            {nightOptions.map((option) => (
+                                <Option key={option} value={option}>
+                                    {option}
+                                </Option>
+                            ))}
+                        </Select>
+                    </div>
+                </div>
+
+                {/* Star Rating */}
+                <div className="filter-item">
+                    <label className="filter-label">{t('star')}</label>
+                    <Radio.Group onChange={(e) => setFilters("rating", e.target.value)} className="filter-radio-group">
+                        {[1, 2, 3, 4, 5].map((value) => (
+                            <Radio key={value} value={`${value}+`}>
+                                {[...Array(value)].map((_, index) => (
+                                    <StarFilled key={index} className="star-icon" />
+                                ))}{" "}
+                                {value}+
+                            </Radio>
                         ))}
-                    </Select>
+                    </Radio.Group>
                 </div>
             </div>
-
-            <HotelConceptSelector />
-
-            <h3 className="text-xs mt-4">{t('star')}</h3>
-            <Radio.Group
-                className="flex flex-col space-y-2 custom-ratio-input"
-                onChange={(e) => setFilters("rating", e.target.value)}
-            >
-                {[1, 2, 3, 4, 5].map((value) => (
-                    <Radio key={value} value={`${value}+`}>
-                        {[...Array(value)].map((_, index) => (
-                            <StarFilled key={index} style={{ color: "#FFA500" }} />
-                        ))}{" "}
-                        {value}+
-                    </Radio>
-                ))}
-            </Radio.Group>
-        </div>
         </>
     );
 };
